5710347bca8d46370feac9e9d21beb4125e36e7f
Abdi Majid <abdi.amajid.dev>
2023-03-07 13:35:13 +0000

    test 2 and 3

--- checkLit.js
+++ checkLit.js
@@ -2,6 +2,26 @@
 const button = document.querySelector("button");
 const toDo = document.getElementById("todo");
 const checklist = document.getElementById("checklist");
+// checkbox dom element
+const checkBox = document.querySelector("#checkbox");
+// p tag dom element
+const todoText = document.querySelector(".todo");
+const task = document.querySelectorAll(".task")
+
+function checked(){
+    checkBox.click();
+    todoText.classList.add("lineThrough")
+}
+
+function deleteTodo(){
+    task[task.length -1].remove();
+    return checklist.childElementCount
+}
+
+deleteTodo()
+
+
+
 
 function submitToDo(text){
   const node = document.createElement("li");
