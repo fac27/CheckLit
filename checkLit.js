2b1a80edd42075b1803788f5dd88bc3afc4fc792
eliazzo <bethazz@hotmail.co.uk>
2023-03-07 13:57:52 +0000

    Template and Test 1 complete

--- checkLit.js
+++ checkLit.js
@@ -1,27 +1,42 @@
+// Access DOM
 const button = document.querySelector("button");
 
-button.addEventListener("click", () => {
-    // Test to see if the browser supports the HTML template element by checking for the presence of the template element's content attribute.
-if ('content' in document.createElement('template')) {
-    // Instantiate the list with the existing HTML tbody and the row with the template
-    const list = document.querySelector("ul");
-    const template = document.querySelector('#checklistrow');
+const checklist = document.querySelector("ul");
 
-    // Access user 'To Do' input value
-    const userInput = document.querySelector("input");
+const userInput = document.querySelector("#todo");
+  
 
-    // Clone the new row and insert it into the table
-    const clone = template.content.cloneNode(true);
-    let li = clone.querySelectorAll("span");
-    // newCheckbox()
-    li[1].textContent = userInput.value;
 
-    list.appendChild(clone);
+function submitToDo(text) {
+
+// Test to see if the browser supports the HTML template element by checking for the presence of the template element's content attribute.
+  if ('content' in document.createElement('template')) {
+
+  // Append the template with the existing HTML <ul> checklist and the row with the template
+    const checklistRow = document.querySelector('#checklistrow');
+
+    // Clone the new row and insert it into the <ul>
+    const clone = checklistRow.content.cloneNode(true); // accesses the content of the <template> element and clones to create a new row
+    let li = clone.querySelectorAll("span"); // accesses the two <span> elements within the new row
+    
+    // Create checkbox
+    const checkbox = document.createElement("input"); 
+    checkbox.type = "checkbox"; // create checkbox
+    li[0].appendChild(checkbox); // append the checkbox to the first <span> element wrapped in <li>
+
+    li[1].textContent = text; // sets the textContent property of the second <span> element to the value of the text parameter.
+
+    checklist.appendChild(clone);
 
 } else {
   // Find another way to add the rows to the table because  the HTML template element is not supported.
 }
 
-})
+}
+
+button.addEventListener("click", () => {
+  submitToDo(`${userInput.value}`)
+});
+
 
 
